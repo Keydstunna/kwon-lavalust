520e5f886660f1e0d2edba5b333c5b906759ee67
Kwonn <kierignacio02@gmail.com>
2026-08-18 19:16:17 +0800

    fix DIR constant typo

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,7 +1,7 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-require_once _DIR_ . '/../middlewares/StudentMiddleware.php';
+require_once __DIR__ . '/../middlewares/StudentMiddleware.php';
 
 class StudentController extends Controller
 {
